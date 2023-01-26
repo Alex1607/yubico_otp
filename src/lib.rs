69bb5734ea980ca6b2bccc81c90d3473b681de12
Alexander <alex@pluoi.com>
2023-01-26 20:02:03 +0100

    fix typos extend Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 //! # YubiOTP
 //!
 //! This crate allows you to verify Yubico OTPs.
-//! To get details about what a Yubico OTP is you can read about it here: <https://developers.yubico.com/OTP/>
+//! To get details about what a Yubico OTP is, you can read about it here: <https://developers.yubico.com/OTP/>
 //!
 //! In order to use this crate you should first check the OTP of the user with the `is_valid_otp` function.
-//! If the OTP has the correct format you can send the OTP to Yubico for verification. To do that you
+//! If the OTP has the correct format, you can send the OTP to Yubico for verification. To do that, you
 //! will have to create a `YubicoClient` with your credentials.
 //! You can then call the `verify` function on it to send the request.
-//! Check if the result has the state `Ok`, if its any other state the OTP is invalid!
+//! Check if the result has the state `Ok`, if it's any other state the OTP is invalid!
 //!
 //! In case the state was `Ok` you will probably also want to check the public id of the key to compare it against the one you have on record.
 //! To get the public id of any OTP you can call the `get_public_id` function.
@@ -42,7 +42,7 @@ pub fn is_valid_otp(otp: &str) -> bool {
         .all(|c| (0x20..=0x7E).contains(&c));
 }
 
-/// Will return the public id of an OTP
+/// Will return the public id of an OTP.
 ///
 /// # Arguments
 ///
